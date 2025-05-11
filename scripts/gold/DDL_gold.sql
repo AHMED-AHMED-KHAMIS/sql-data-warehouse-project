a88454daf349df9e0f0d50a0f6d1d8216985acb7
AHMED AHMED KHAMIS <116842742+AHMED-AHMED-KHAMIS@users.noreply.github.com>
2025-05-11 21:28:00 +0300

    Update DDL_gold.sql

--- scripts/gold/DDL_gold.sql
+++ scripts/gold/DDL_gold.sql
@@ -51,23 +51,23 @@ IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
     DROP VIEW gold.dim_products;
 GO
 
-CREATE VIEW gold.dim_products AS
-SELECT
-    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS products_key, -- Surrogate key
-    pn.prd_id       AS product_id,
-    pn.prd_key      AS product_key,
-    pn.prd_nm       AS product_number,
-    pn.cat_id       AS category_id,
-    pc.cat          AS category,
-    pc.subcat       AS subcategory,
-    pc.maintenance  AS maintenance,
-    pn.prd_cost     AS cost,
-    pn.prd_line     AS product_line,
-    pn.prd_start_dt AS start_date
-FROM silver.crm_prd_info pn
-LEFT JOIN silver.erp_px_cat_g1v2 pc
-    ON pn.cat_id = pc.id
-WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
+CREATE VIEW gold.dim_products AS 
+	SELECT 
+		ROW_NUMBER() OVER(ORDER BY prd_key,prd_start_dt) AS product_key,-- To create a Surrogate key
+		pri.[prd_id] AS product_id,
+		pri.[prd_key] AS product_number,
+		pri.[prd_nm]AS product_name,
+		pri.[category_id],
+		pcg.[CAT] AS category,
+		pcg.[SUBCAT] AS subcategory,
+		pcg.[MAINTENANCE] AS maintenance,
+		pri.[prd_cost] AS product_cost,
+		pri.[prd_line] AS product_line,
+		pri.[prd_start_dt] AS Product_start_date,
+		pri.[prd_end_dt] AS Product_end_date 
+	FROM [silver].[crm_prd_info] AS pri
+	LEFT JOIN [silver].[erp_PX_CAT_G1V2] AS pcg
+	ON pri.category_id = pcg.ID;
 GO
 
 -- =============================================================================
@@ -77,20 +77,20 @@ IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
     DROP VIEW gold.fact_sales;
 GO
 
-CREATE VIEW gold.fact_sales AS
+CREATE VIEW gold.fact_sales AS 
 SELECT
-    sd.sls_ord_num  AS order_number,
-    pr.product_key  AS product_key,
-    cu.customer_key AS customer_key,
-    sd.sls_order_dt AS order_date,
-    sd.sls_ship_dt  AS shipping_date,
-    sd.sls_due_dt   AS due_date,
-    sd.sls_sales    AS sales_amount,
-    sd.sls_quantity AS quantity,
-    sd.sls_price    AS price
-FROM silver.crm_sales_details sd
-LEFT JOIN gold.dim_products pr
-    ON sd.sls_prd_key = pr.product_number
-LEFT JOIN gold.dim_customers cu
-    ON sd.sls_cust_id = cu.customer_id;
+	sd.[sls_ord_num] AS order_number,
+	dp.product_key, -- using the Dim_product Surrogate Key 
+	dc.customer_key, -- using the Dim_customer Surrogate Key 
+	sd.[sls_order_dt] AS order_date,
+	sd.[sls_ship_dt] AS shipping_date,
+	sd.[sls_due_dt] AS due_date,
+	sd.[sls_sales] AS sales_amount,
+	sd.[sls_quantity] As quantity,
+	sd.[sls_price] AS price
+FROM [silver].[crm_sales_details] AS sd
+LEFT JOIN gold.dim_products AS dp
+	ON sd.sls_prd_key= dp.product_number
+LEFT JOIN gold.dim_customers dc
+	ON sd.sls_cust_id=dc.customer_id;
 GO
